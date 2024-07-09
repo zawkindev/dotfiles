52751cfbb75f8b0af47fa28d2d36fe4794d19f56
zawkindev <zawkindev@gmail.com>
2024-07-09 10:29:25 +0500

    feat: add treesitter

--- nvim/lua/asuna/maps.lua
+++ nvim/lua/asuna/maps.lua
@@ -1,2 +1,32 @@
-vim.g.mapleader = ";"
-vim.keymap.set("n", "<leader>q", vim.cmd.Ex)
+vim.g.mapleader = " "
+vim.keymap.set("n", ";q", vim.cmd.Ex)
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
+
+vim.keymap.set("n", "<leader>f", function()
+  vim.lsp.buf.format()
+end)
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Split window
+vim.keymap.set("n", "ss", ":split<Return>", { noremap = true, silent = true })
+vim.keymap.set("n", "sv", ":vsplit<Return>", { noremap = true, silent = true })
+-- Move window
+vim.keymap.set("n", "sh", "<C-w>h")
+vim.keymap.set("n", "sk", "<C-w>k")
+vim.keymap.set("n", "sj", "<C-w>j")
+vim.keymap.set("n", "sl", "<C-w>l")
--- nvim/lua/asuna/plugins/telescope.lua
+++ nvim/lua/asuna/plugins/telescope.lua
@@ -3,8 +3,8 @@ return {
       dependencies = { 'nvim-lua/plenary.nvim' },
       config=function()
 	local builtin = require('telescope.builtin')
-	vim.keymap.set('n', '<leader>f', builtin.find_files, {})
-	vim.keymap.set('n', '<leader>r', builtin.live_grep, {})
+	vim.keymap.set('n', ';f', builtin.find_files, {})
+	vim.keymap.set('n', ';r', builtin.live_grep, {})
       end
 }
 
